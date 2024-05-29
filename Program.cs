7f0e3866ce0c067718e3dcfe26f765171799a217
fs-simonb <Bruegel@fs-net.de>
2024-05-29 14:27:04 +0200

    added hello_debug example application.  User can start directly with remote debugging

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        for (int i = 0; i < 10; ++i)
+        {
+            Console.WriteLine($"Running... {i + 1}");
+            Thread.Sleep(1000);
+        }
+
+        Console.WriteLine("Finished running.\n");
+    }
+}
